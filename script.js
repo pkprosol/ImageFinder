04942f7fc9af6de27233faf05334a603f18fdf29
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 20:59:45 -0500

    Window open

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		function openAuthenticationWin()
 		{
 			var myWindow = window.open("https://api.instagram.com/oauth/authorize/?client_id=e4bd275464ca4eaeb2eefa4bd253f2b2
-										&redirect_uri='http://pkprosol.github.io/ImageFinder/&response_type=code'");
+										&redirect_uri=http://pkprosol.github.io/ImageFinder/&response_type=code");
 		}
 		openAuthenticationWin();
 		$.getJSON("https://api.instagram.com/v1/tags/snow/media/recent?max_id=0&min_id=10", function(json) {
