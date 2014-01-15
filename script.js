fc1dbfac41141afdefec21a8f7cfbaf2e2175146
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 22:47:01 -0500

    Ajax link

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ $(document).ready(function() {
 	$.ajax({
 		type: "POST",
 		datatype: 'json',
+		link: "https://api.instagram.com/oauth/access_token",
 		client_id: "e4bd275464ca4eaeb2eefa4bd253f2b2",
 		client_secret: "5f80c997ad7f4230968d3c44c111f7e8",
 		grant_type: "authorization_code",
 		redirect_uri: "http://pkprosol.github.io/ImageFinder/",
-		code: URLcode
+		code: URLcode 
 	});
 
 	$("#searchButton").click(function() {
