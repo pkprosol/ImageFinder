76409eb7b774df6d05dbc7a2f090cd9b79b00e24
Peter Prosol <peter.prosol@gmail.com>
2014-01-15 06:44:17 -0500

    Login logic

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-	$('#login').click(function() {
-        var currentURL = document.URL;
-        var pathArray = currentURL.split('access_token=');
-        var accessToken = pathArray[1];
-        console.log("Access Token: " + accessToken);
-        if(accessToken === undefined) {
-            $('.notices').html("Unfortunately you must log in to use this app");
-        } else {
-            $('#login').hide();
-            var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
-            var access = {access_token:accessToken};
-        }
-    });
+    var currentURL = document.URL;
+    var pathArray = currentURL.split('access_token=');
+    var accessToken = pathArray[1];
+    console.log("Access Token: " + accessToken);
+    if(accessToken === undefined) {
+        $('.notices').html("Unfortunately you must log in to use this app");
+    } else {
+        $('#login').hide();
+        var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
+        var access = {access_token:accessToken};
+    }
     
     function getImages(accessInput, tagInput, clickcount) {
         console.log(accessInput);
