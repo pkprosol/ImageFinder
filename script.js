fcec6b52f3fdeeb8dc4372392fca9380a6574be3
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 23:24:23 -0500

    Authentication issues

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 $(document).ready(function() {
 	var currentURL = document.URL;
-	var pathArray = currentURL.split('?code=');
+	var pathArray = currentURL.split('?token=');
 	var URLcode = pathArray[1];
 	console.log(URLcode);
 	console.log("Hello");
 
 	$("#searchButton").click(function() {
-		console.log($.POST("https://api.instagram.com/oauth/access_token",
-			{
-				client_id: "e4bd275464ca4eaeb2eefa4bd253f2b2",
-				client_secret: "5f80c997ad7f4230968d3c44c111f7e8",
-				grant_type: "authorization_code",
-				redirect_uri: "http://pkprosol.github.io/ImageFinder/",
-				code: URLcode 
-			},
-			console.log("Success")
-  		);
-  		);
 		var userInput = $("#userTag").val();
 		console.log(userInput);	
 	});
