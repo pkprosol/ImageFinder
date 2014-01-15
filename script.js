da57e770d3c001a2b3be8127994ad0104f395fe6
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 19:40:38 -0500

    Comments

--- script.js
+++ script.js
@@ -6,20 +6,4 @@ $(document).ready(function() {
 });
 
 
-/* $(document).ready(function() {
-	$.ajax({
-		type: "GET",
-		dataType: "jsonp",
-		cache: false,
-		url: "https://api.instagram.com/v1/tags/winter/media/recent",
-		success: function(response) {
-  			for (var i=0; i<25; i++)  
-  				{
-    				$("#photos").html(response.data[i].comments.from.username);
-  				}		
-  			}
-		});
-	});
-});
 
-*/
