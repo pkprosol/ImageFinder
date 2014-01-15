a0baf8d74b2797fa17afff281b12892ede6be577
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 23:36:06 -0500

    Access token and test API get

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	var currentURL = document.URL;
 	var pathArray = currentURL.split('#access_token=');
-	var URLcode = pathArray[1];
-	console.log(URLcode);
+	var accessToken = pathArray[1];
+	console.log(accessToken);
 	console.log("Hello");
 
 	$("#searchButton").click(function() {
 		var userInput = $("#userTag").val();
 		console.log(userInput);	
+		console.log("https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken);
 	});
 
 });
