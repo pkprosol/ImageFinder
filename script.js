4c83097b77e67b62028c05c36a6bec7c07c413e5
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 21:02:57 -0500

    Link

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	$("#searchButton").click(function() {
 		var userInput = $("#userTag").val();
 		console.log(userInput);
-		window.open("https://api.instagram.com/oauth/authorize/?client_id=e4bd275464ca4eaeb2eefa4bd253f2b2
-										&redirect_uri=http://pkprosol.github.io/ImageFinder/&response_type=code");
+		
 	
 	});
 });
