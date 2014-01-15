c9bab123cdbee54bd42af47a8396a28c7d009775
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 23:13:29 -0500

    Fixed post

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ $(document).ready(function() {
 	console.log(URLcode);
 	console.log("Hello");
 
- 	$.POST("https://api.instagram.com/oauth/access_token",
-		{
-			client_id: "e4bd275464ca4eaeb2eefa4bd253f2b2",
-			client_secret: "5f80c997ad7f4230968d3c44c111f7e8",
-			grant_type: "authorization_code",
-			redirect_uri: "http://pkprosol.github.io/ImageFinder/",
-			code: URLcode 
-		},
-		console.log("Success")
-  	});
-
 	$("#searchButton").click(function() {
+		$.POST("https://api.instagram.com/oauth/access_token",
+			{
+				client_id: "e4bd275464ca4eaeb2eefa4bd253f2b2",
+				client_secret: "5f80c997ad7f4230968d3c44c111f7e8",
+				grant_type: "authorization_code",
+				redirect_uri: "http://pkprosol.github.io/ImageFinder/",
+				code: URLcode 
+			},
+			console.log("Success")
+  		);
 		var userInput = $("#userTag").val();
 		console.log(userInput);	
 	});
