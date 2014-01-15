6c823163ae44cb74922c77f0f1e249f8df17f09b
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 21:59:12 -0500

    Got code from URL

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+	var currentURL = document.URL;
+	var pathArray = currentURL.split('?code=');
+	var code = pathArray[1];
+	console.log(code);
+
 	$("#searchButton").click(function() {
 		var userInput = $("#userTag").val();
 		console.log(userInput);	
-		console.log(document.URL);
 	});
 
 });
