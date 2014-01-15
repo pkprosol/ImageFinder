c00339065de7d90973a4f23bf9450d5e6ba82495
Peter Prosol <peter.prosol@gmail.com>
2014-01-15 05:38:17 -0500

    Separation of function and call

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ $(document).ready(function() {
 	var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
     var access = {access_token:accessToken};
 
+    function getImages(accessInput, tagInput) {
+        var apiURL = 'https://api.instagram.com/v1/tags/' + tagInput + '/media/recent?callback=?';
+        $.getJSON(apiURL, accessInput, function(data) {
+            console.log(data.data[0].images.standard_resolution.url);
+            $(".photos").html("<img src='" + data.data[0].images.standard_resolution.url + "'>");
+        });
+    }
+
     $('#searchButton').click(function() {
         var tag = $('#userTag').val();
         console.log("Tag: " + tag);
-        
-        function getImages(accessInput) {
-            var apiURL = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?';
-            $.getJSON(apiURL, accessInput, function(data) {
-                console.log(data.data[0].images.standard_resolution.url);
-                $(".photos").html("<img src='" + data.data[0].images.standard_resolution.url + "'>");
-            });
-        }
 
-        getImages(access);
+        getImages(access, tag);
     });
 
 });
