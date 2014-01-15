501987860b99f8a7d5013286f63317ead7f37b6e
Peter Prosol <peter.prosol@gmail.com>
2014-01-14 21:39:26 -0500

    API updates

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+	window.open("https://api.instagram.com/oauth/authorize/?client_id=e4bd275464ca4eaeb2eefa4bd253f2b2&redirect_uri=http://pkprosol.github.io/ImageFinder/&response_type=code");
 	$("#searchButton").click(function() {
 		var userInput = $("#userTag").val();
-		console.log(userInput);
-		window.open("https://api.instagram.com/oauth/authorize/?client_id=e4bd275464ca4eaeb2eefa4bd253f2b2&redirect_uri=http://pkprosol.github.io/ImageFinder/&response_type=code");
+		console.log(userInput);	
+		console.log(document.URL);
 	});
 });
 
