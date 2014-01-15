89043edd9400ca9e6a5c6724f7891c8fbebc5802
Peter Prosol <peter.prosol@gmail.com>
2014-01-15 03:04:14 -0500

    Jquery

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 $(document).ready(function() {
 	var currentURL = document.URL;
-	var pathArray = currentURL.split('?code=');
-	var authCode = pathArray[1];
-	console.log(authCode);
-	console.log("Hello");
+	var pathArray = currentURL.split('access_token=');
+	var accessToken = pathArray[1];
+	console.log("Access Token" + accessToken);
 
-	var postLink = "https://api.instagram.com/oauth/access_token=" + authCode;
-	console.log(postLink);
+	var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
 
-	var posting = $.post(postLink, 
-		{ 
-			client_id: "e4bd275464ca4eaeb2eefa4bd253f2b2",
-			client_secret: "5f80c997ad7f4230968d3c44c111f7e8",
-			grant_type: "authorization_code",
-			redirect_uri: "http://pkprosol.github.io/ImageFinder/",
-			code: authCode
-		});
-
-	console.log(posting);
-		
+	$.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: getURL,
+        success: function(data) {
+        	console.log(data);
+    	})
+	});
 });
-
 
 
 
