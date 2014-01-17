89b65dcf61c07960c9090027ebe89ef3b30bd221
Peter Prosol <peter.prosol@gmail.com>
2014-01-17 14:48:51 -0500

    Styling

--- script.js
+++ script.js
@@ -2,36 +2,42 @@ $(document).ready(function() {
     var currentURL = document.URL;
     var pathArray = currentURL.split('access_token=');
     var accessToken = pathArray[1];
+    var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
+    var access = {access_token:accessToken};
+    var counter = 0;
+    var tag = []; 
+    var urlArray = [];
+
     console.log("Access Token: " + accessToken);
     if(accessToken !== undefined) {
         $('#login').hide();
     }
-    var getURL = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=" + accessToken;
-    var access = {access_token:accessToken};
-    
+
+// wrap getjson in function; run recursion
+// indexof
+
     function getImages(accessInput, tagInput, clickcount) {
         console.log("GetImages ran");
         console.log("Round: " + clickcount);
         console.log("AccessInput: " + accessInput);
         console.log("Tag: " + tagInput);
-        var apiURL = 'https://api.instagram.com/v1/tags/' + tagInput + '/media/recent?callback=?';
-        $.getJSON(apiURL, accessInput, function(data) {
+        var apiURL = 'https://api.instagram.com/v1/tags/' + tagInput + '/media/recent?callback=?'; // can say JSON request done; may populate
+        $.getJSON(apiURL, accessInput, function(data) { // I can name this response/ response.data; return is encapsulated; generic function
             console.log("Data.data: " + data.data);
             console.log("isEmptyObject(data.data): " + jQuery.isEmptyObject(data.data));
             if (jQuery.isEmptyObject(data.data)) {
                 $('.notices').html("Sorry, not an active tag, try again.");
                 console.log("Data undefined, should be error");
             } else {
-                var result = data.data[clickcount].images.standard_resolution.url; 
+                var result = data.data[clickcount].images.standard_resolution.url;  // callback function; PHP success function would be involved; jQuery has success callback built in
                 console.log("Data defined. Result is: " + result);
                 $('.photos').html("<img src='" + result + "' height='400' width='400'>");
+                urlArray.push(result);
+                console.log(urlArray);
             } 
             console.log("If statement ran");
         });
     }
-
-    var counter = 0;
-    var tag = [];
 
     $('#searchButton').click(function() {
         console.log("searchButton was clicked");
