a67f652b9f72450105b25d8fa7231f5dc0be572e
Peter Prosol <peter.prosol@gmail.com>
2014-01-19 14:27:15 -0500

    Various

--- script.js
+++ script.js
@@ -17,82 +17,70 @@ $(document).ready(function() {
 // indexof
 // pagination
 
-    function getImages(accessInput, tagInput, clickcount) {
-        console.log("GetImages ran");
-        console.log("Round: " + clickcount);
-        console.log("AccessInput: " + accessInput);
-        console.log("Tag: " + tagInput);
+    function getImages(accessInput, tagInput) {
+
         var apiURL = 'https://api.instagram.com/v1/tags/' + tagInput + '/media/recent?callback=?'; // can say JSON request done; may populate
        
-        $.getJSON(apiURL, accessInput, function(data) { // I can name this response/ response.data; return is encapsulated; generic function
-            console.log("Data.data: " + data.data);
-            console.log("isEmptyObject(data.data): " + jQuery.isEmptyObject(data.data));
-            if (jQuery.isEmptyObject(data.data)) {
-                var notTagError = 'Sorry, not an active tag, try again.<br>';
-                $('.notices').html(notTagError);
-                console.log("Data undefined, should be error");
-            } else {
-                var result = data.data[clickcount].images.standard_resolution.url;  // callback function; PHP success function would be involved; jQuery has success callback built in
-                console.log("Result URL: " + result); 
+        if (urlArray.length === 20) {
+            $('.notices').html("Sorry, please pick a new key word");
+        } else {
+            $.getJSON(apiURL, accessInput, function(data) { // I can name this response/ response.data; return is encapsulated; generic function
+
+                if (jQuery.isEmptyObject(data.data)) {
+                    $('.notices').html('Sorry, not an active tag, try again.');
+
+                } else {
+                    var result = data.data[clickcount].images.standard_resolution.url;  // callback function; PHP success function would be involved; jQuery has success callback built in
+                    console.log("Result URL: " + result); 
                            
-                showNewPhoto(result);
-            }  
-        });
-    }
+                    showNewPhoto(result);
+                }  
+            });
+        }
+    }   
 
     function showNewPhoto(resultURL) {
         
-        urlArray.push(resultURL);
         console.log("URL Array length: " + urlArray.length);
         console.log("URL Array: " + urlArray);
 
         var duplicateScore = 0;
         
-        for (i=0; i<(urlArray.length-1); i++) {
+        for (i=0; i<(urlArray.length); i++) {
             if (urlArray[i] === resultURL) {
                 duplicateScore = ++duplicateScore;
             }
         }
-        
+
         console.log("Duplicate Score: " + duplicateScore);
 
+        urlArray.push(resultURL);
+        
         if (duplicateScore > 0) {
-            getNextPhoto();
+            getImages(access, tag);
         } else {
             $('.photos').html("<img id='currentImage' src='" + resultURL + "' height='400' width='400'>");
         }
     }
 
-    function getNextPhoto () {
-        counter = counter + 1;
-        console.log("Counter in .photos.click(): " + counter);
-        if (counter > 19) {
-            $('.notices').html("Sorry, please pick a new key word");
-        } else {
-            getImages(access, tag, counter);
-        }
-    }  
-
     $('#searchButton').click(function() {
         console.log("searchButton was clicked");
         $('.notices').html("");
         tag = $('#userTag').val();
-        console.log("Tag: " + tag);
-        console.log("Typeof tag: " + typeof tag);
-        counter = 0;
+
         urlArray = [];
+
         if (accessToken === undefined) {
             $('.notices').html("You must log in first.");
         } else if (tag === "") {
             $('.notices').html("You must search a tag to get photos.");
         } else {
-            getImages(access, tag, counter);     
+            getImages(access, tag);     
         }
     });
 
     $('.photos').click(function() {
-        console.log(".photos was clicked");
-        getNextPhoto();
+        getImages(access, tag);
     });     
 
 });
